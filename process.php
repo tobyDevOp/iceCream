bd11308d3deb30224cfba590b51af55f79fc5f14
TobyDevOps <Tngoetz@aol.com>
2024-01-09 13:48:59 -0500

    Fixed Empty Array Error

--- process.php
+++ process.php
@@ -24,7 +24,14 @@
     var_dump($_POST);
     echo "</pre>";
 
-    $scoops = $_POST['scoops'];
+    //Get data from POST array
+    if (!empty($scoops)) {
+        $scoops = $_POST['scoops'];
+    } else {
+        echo "<p>Enter Scoops!</p>";
+        return;
+    }
+
     $flavors = $_POST['flavor'];
     $flavorString = implode(", ", $flavors);
     $cone = $_POST['cone'];
